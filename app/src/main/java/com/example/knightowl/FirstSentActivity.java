5c94ceda028f1ee7049169859827590ce227024c
adscheerer <alexisscheerer@gmail.com>
2019-04-07 01:16:51 -0700

    java for current activities done

--- app/src/main/java/com/example/knightowl/FirstSentActivity.java
+++ app/src/main/java/com/example/knightowl/FirstSentActivity.java
@@ -1,29 +1,37 @@
 package com.example.knightowl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class FirstSentActivity extends AppCompatActivity {
+    private Bundle b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_sent);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        b = getIntent().getExtras();
     }
+
+    public void help(View view)
+    {
+        Intent intent = new Intent(this, FirstSentActivity.class); // to be changed
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
+    public void isBack(View view)
+    {
+        Intent intent = new Intent(this, FirstSentActivity.class); // to be changed
+        intent.putExtras(b);
+        startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/example/knightowl/TransportFormActivity.java
+++ app/src/main/java/com/example/knightowl/TransportFormActivity.java
@@ -1,25 +1,41 @@
 package com.example.knightowl;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class TransportFormActivity extends AppCompatActivity {
-    private String phone1;
-    private String phone2;
-    private String phone3;
+    private Bundle b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport_form);
-        Bundle extras = getIntent().getExtras();
-        phone1 = extras.getString("p1");
-        phone2 = extras.getString("p2");
-        phone2 = extras.getString("p3");
+        b = getIntent().getExtras();
+    }
+
+    public void submit(View view)
+    {
+        Intent intent = new Intent(this, FirstSentActivity.class);
+        EditText start = findViewById(R.id.start);
+        EditText dest = findViewById(R.id.dest);
+        EditText name = findViewById(R.id.driver_name);
+        EditText license = findViewById(R.id.license_plate);
+        String value1 = start.getText().toString();
+        String value2 = dest.getText().toString();
+        String value3 = name.getText().toString();
+        String value4 = license.getText().toString();
+        b.putString("start", value1);
+        b.putString("dest", value2);
+        b.putString("name", value3);
+        b.putString("license", value4);
+        intent.putExtras(b);
+        startActivity(intent);
     }
 
 }
