329ab229d9aee642ccd7699e55a7ac4ae813e08a
Alicia Leung <aliciazoeleung@gmail.com>
2019-04-06 17:38:05 -0700

    First 2 Actvities

---
+++ app/src/main/java/com/example/knightowl/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.knightowl;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void start(View view)
+    {
+        Intent intent = new Intent(this, FriendFormActivity.class);
+        startActivity(intent);
+    }
+}
